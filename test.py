14d245a98cc27f0822331363b5cc59308f62c3d2
VibhaPushparaja <vibha.pushparaja@accenture.com>
2025-11-27 06:47:48 +0000

    Print "Hello World!" and "Hello (name)!"

---
+++ test.py
@@ -0,0 +1,5 @@
+# write a program that prints Hello World
+print("Hello World")
+#Take input form user asking his/her name and print the name
+name = input("Please enter your name: ")
+print("Hello, " + name + "!")
